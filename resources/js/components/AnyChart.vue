d1e3b912f3f3751124f29bc2c46ce61279f1eef1
Petr <savinkin@bk.ru>
2020-11-23 18:07:40 +0500

    update 18:07

--- resources/js/components/AnyChart.vue
+++ resources/js/components/AnyChart.vue
@@ -2,27 +2,33 @@
 
 <script>
      import {Doughnut} from 'vue-chartjs'
+     import labels from 'chartjs-plugin-labels'
+     Vue.use(labels)
 
      export default {
          extends: Doughnut,
+         plugins:{
+             labels:[
+                 {render: 'value'}
+             ]
+         },
          props:{
              masiv:{
-                 type: Array,
-                 default: 1
+                 type:Array,
+                 default: null
              },
              headers:{
-                 type: Array,
-                 default: 2
+                 type:Array,
+                 default: null
              },
          },
          watch:{
              masiv(){
                  console.log('Из AnyChart')
-                 //console.log(this.masiv)
-
                  this.renderChart({
+                     labels: this.headers,
                      datasets: [{
-                         label:'votex',
+                         label: 'Данные 1',
                          data: this.masiv,
                          backgroundColor: [
                              'rgba(255, 99, 132, 1)',
@@ -34,42 +40,43 @@
                              'rgba(0, 32, 22, 255)',
                          ],
                          /*borderColor: [
-                             'rgba(255, 99, 132, 1)',
-                             'rgba(54, 162, 235, 1)',
-                             'rgba(255, 206, 86, 1)',
-                         ],*/
-                         borderWidth: 1
-                     }],
-                     // These labels appear in the legend and in the tooltips when hovering different arcs
-                     labels: this.headers,
-                 })
-             }
-         },
-         mounted () {
-             // Overwriting base render method with actual data.
-             /*this.renderChart({
-                 datasets: [{
-                     data: this.masiv,
-                     backgroundColor: [
-                         'rgba(255, 99, 132, 0.2)',
-                         'rgba(54, 162, 235, 0.2)',
-                         'rgba(255, 206, 86, 0.2)',
-                     ],
-                     borderColor: [
                          'rgba(255, 99, 132, 1)',
                          'rgba(54, 162, 235, 1)',
                          'rgba(255, 206, 86, 1)',
+                     ],*/
+                         borderWidth: 1
+                     }],
+                     responsive: true,
+                     maintainAspectRatio: false
+                 })
+
+             }
+         },
+         mounted () {
+             this.renderChart({
+                 labels: this.headers,
+                 datasets: [{
+                     label: 'Данные 1',
+                     data: this.masiv,
+                     backgroundColor: [
+                         'rgba(255, 99, 132, 1)',
+                         'rgba(54, 162, 235, 1)',
+                         'rgba(255, 206, 86, 1)',
+                         'rgba(28, 255, 1, 255)',
+                         'rgba(173, 0, 255, 255)',
+                         'rgba(0, 255, 180, 255)',
+                         'rgba(0, 32, 22, 255)',
                      ],
+                     /*borderColor: [
+                     'rgba(255, 99, 132, 1)',
+                     'rgba(54, 162, 235, 1)',
+                     'rgba(255, 206, 86, 1)',
+                 ],*/
                      borderWidth: 1
                  }],
-                 // These labels appear in the legend and in the tooltips when hovering different arcs
-                 labels: this.headers
-             })*/
-
+                 responsive: true,
+                 maintainAspectRatio: false
+             })
          },
-         /*updated() {
-             console.log(this.masiv)
-             console.log('update')
-         }*/
      }
 </script>
